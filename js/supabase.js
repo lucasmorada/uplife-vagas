09b353a68f16c570339fa59c13506c8f3aa2ce6f
Lucas Gabriel <lucasgab.siqueira@gmail.com>
2026-06-22 16:04:18 -0300

    inseri imagem

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+// Etapa 2:
+// Aqui vamos configurar o cliente Supabase.
+
+// Exemplo futuro:
+// const SUPABASE_URL = "SUA_URL_DO_SUPABASE";
+// const SUPABASE_ANON_KEY = "SUA_ANON_KEY_DO_SUPABASE";
+//
+// const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
