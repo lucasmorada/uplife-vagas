9b2a4479c34a2e57f1c43ac88f4ab7997cfc6c4f
Lucas Gabriel <lucasgab.siqueira@gmail.com>
2026-06-22 17:20:18 -0300

    supabase

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = "https://ptdxkhkbxcgtfclqgndn.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_G8YH1t0PV8TROwJcT5Vwsw_thAE--je";
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
